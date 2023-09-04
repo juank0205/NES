b5eceeea854afbd5822a6395bf7b1336a9447741
juank0205 <jkamiloeche@outlook.com>
2023-09-04 17:36:58 -0500

    addressing modes

--- NES/src/olc6502.cpp
+++ NES/src/olc6502.cpp
@@ -225,10 +225,7 @@ uint8_t olc6502::ABX() {
   addr_abs = (hi << 8) | lo;
   addr_abs += x;
 
-  if ((addr_abs & 0xFF00) != (hi << 8))
-    return 1;
-  else
-    return 0;
+  return (addr_abs & 0xFF00) != (hi << 8) ? 1 : 0;
 }
 
 uint8_t olc6502::ABY() {
@@ -240,10 +237,7 @@ uint8_t olc6502::ABY() {
   addr_abs = (hi << 8) | lo;
   addr_abs += y;
 
-  if ((addr_abs & 0xFF00) != (hi << 8))
-    return 1;
-  else
-    return 0;
+  return (addr_abs & 0xFF00) != (hi << 8) ? 1 : 0;
 }
 
 uint8_t olc6502::IND() {
@@ -254,7 +248,45 @@ uint8_t olc6502::IND() {
 
   uint16_t ptr = (ptr_hi << 8) | ptr_lo;
 
-  addr_abs = (read(ptr + 1) << 8) | read(ptr + 0);
-
+  if (ptr_lo == 0x00FF) addr_abs = (read(ptr & 0x00FF) << 8) | read(ptr + 0);
+  else addr_abs = (read(ptr + 1) << 8) | read(ptr + 0);
+  
   return 0;
+}
+
+uint8_t olc6502::IZX()
+{
+    uint16_t t = read(pc);
+    pc++;
+
+    uint16_t lo = read((uint16_t)(t + (uint16_t)x) & 0x00FF);
+    uint16_t hi = read((uint16_t)(t + (uint16_t)x + 1) & 0x00FF);
+
+    addr_abs = (hi << 8) | lo;
+
+    return 0;
+}
+
+uint8_t olc6502::IZY()
+{
+    uint16_t t = read(pc);
+    pc++;
+
+    uint16_t lo = read(t & 0x00FF);
+    uint16_t hi = read((t + 1) & 0x00FF);
+
+    addr_abs = (hi << 8) | lo;
+    addr_abs += y;
+
+    return (addr_abs & 0x00FF) != (hi << 8) ? 1 : 0;
+}
+
+
+uint8_t olc6502::REL()
+{
+    addr_rel = read(pc);
+    pc++;
+    if (addr_rel & 0x80)
+        addr_rel |= 0xFF00;
+    return 0;
 }
